910e4a6812745356f9c1c0adce11cd9ad77fcae8
寂寞酱 <jimowutong888@qq.com>
2021-04-19 13:21:14 +0800

    update birthday/main.js.

--- birthday/main.js
+++ birthday/main.js
@@ -3,7 +3,6 @@ $.ajax({
     type:"post"
     data:"forType=Studio&forId=64161&content=祝寂寞酱破蛋日快乐~q&t=1621401586738&s=cb05d90e5755347a671bef81397f36dcb7a0e69a"
     success:function(data){
-         //获取接口返回的数据
          console.log(data)
     }
 })
