3970bebf7016022bd997e7b74afaaacbee8c8688
寂寞酱 <jimowutong888@qq.com>
2021-04-19 13:40:49 +0800

    update birthday/main.js.

--- birthday/main.js
+++ birthday/main.js
@@ -1,8 +1,11 @@
-$.ajax({
-    url:"https://gitblock.cn/WebApi/Comment/Post"
-    type:"post"
-    data:"forType=Studio&forId=64161&content=祝寂寞酱破蛋日快乐~q&t=1621401586738&s=cb05d90e5755347a671bef81397f36dcb7a0e69a"
-    success:function(data){
-         console.log(data)
-    }
-})
+function Post()
+{
+    $.ajax({
+        url:"https://gitblock.cn/WebApi/Comment/Post"
+        type:"post"
+        data:"forType=Studio&forId=64161&content=祝寂寞酱破蛋日快乐~q&t=1621401586738&s=cb05d90e5755347a671bef81397f36dcb7a0e69a"
+        success:function(data){
+            console.log(data)
+        }
+    })
+}
