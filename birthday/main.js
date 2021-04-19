2f6093ea54595af7ee5e575061dba8a572fcd5f6
寂寞酱 <jimowutong888@qq.com>
2021-04-19 13:20:55 +0800

    update birthday/main.js.

--- birthday/main.js
+++ birthday/main.js
@@ -1,7 +1,7 @@
 $.ajax({
     url:"https://gitblock.cn/WebApi/Comment/Post"
     type:"post"
-    data:"forType=Studio&forId=64161&content=qwq&t=1621401586738&s=cb05d90e5755347a671bef81397f36dcb7a0e69a"
+    data:"forType=Studio&forId=64161&content=祝寂寞酱破蛋日快乐~q&t=1621401586738&s=cb05d90e5755347a671bef81397f36dcb7a0e69a"
     success:function(data){
          //获取接口返回的数据
          console.log(data)
