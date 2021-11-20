15096b009d8ae1824ac1de675dd1a9196359de03
寂寞酱 <jimowutong888@qq.com>
2021-11-20 17:45:36 +0000

    update loving/js/index.js.

--- loving/js/index.js
+++ loving/js/index.js
@@ -1,4 +1,4 @@
-var arr = "2021-11-18 00:00:00".split(/[- :]/);//改这里
+var arr = "2021-11-18 00:00:00".split(/[- :]/);
 var box = document.getElementsByClassName('textCon')[0];
 function init() {
     var width = box.offsetWidth;
